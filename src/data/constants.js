2604834f695681beefc403656113dfd181f7d2bb
DSStyluxe <141305893+DSStyluxe@users.noreply.github.com>
2023-12-18 21:45:45 +0800

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,169 @@
+export const Bio = {
+  name: "Ynoob",
+  roles: [
+    "am a Full Stack Developer",
+    "am a Game Programmer",
+    "Play Osu"
+  ],
+  description: "HELLO",
+  github: "",
+  resume: "https://osu.ppy.sh/users/30500236",
+  linkedin: "",
+  twitter: "",
+  insta: "",
+  facebook: "",
+};
+
+export const skills = [
+  {
+    title: "Game Engine",
+    skills: [
+      {
+        name: "Roblox Studio",
+        image: "https://devforum-uploads.s3.dualstack.us-east-2.amazonaws.com/uploads/original/4X/6/2/f/62f64963b3b8eda573996bdfb646729e818ef77b.png",
+      },
+    ],
+  },
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "Lua",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Lua-Logo.svg/600px-Lua-Logo.svg.png?20150107024942",
+      },
+      {
+        name: "Luau",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Luau_Logo_%28Programming_Language%29.svg/2048px-Luau_Logo_%28Programming_Language%29.svg.png",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Lua",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Lua-Logo.svg/600px-Lua-Logo.svg.png?20150107024942",
+      },
+      {
+        name: "Luau",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Luau_Logo_%28Programming_Language%29.svg/2048px-Luau_Logo_%28Programming_Language%29.svg.png",
+      },
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.discordapp.net/attachments/984622296318758913/1165311363606917241/Group_2_8.png?ex=659037c7&is=657dc2c7&hm=fe7a5ce791ad9af140ed931b826c52f6fd9dd1a365dd8304874cb5c94b0926db&=&format=webp&quality=lossless&width=448&height=448",
+    role: "FPS Game Development",
+    company: "CR2 Studio",
+    date: "2023/12/1",
+    desc: "Game Development",
+    skills: [
+      "Roblox Studio",
+      "Lua",
+      "Luau",
+    ],
+    doc: "",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://sao.tpcu.edu.tw/ezfiles/29/1029/pictures/684/part_20960_5772799_04457.jpg",
+    school: "Taipei City University of Science & Technology,TPCU",
+    date: "Oct 2023 - Sep 2028",
+    grade: "First grade of high school",
+    desc: "Department of Computer Science and Information Engineering",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Horror Game",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "Horror game first person view model",
+    image:
+      "https://media.discordapp.net/attachments/1148669124180983913/1178701228167938099/image.png?ex=65898f10&is=65771a10&hm=2c70213b7e74c7236b614421702a9b9576764930fd3d1dd003a55cdd2f257a0e&=&format=webp&quality=lossless&width=786&height=448",
+    tags: [
+      "Roblox Studio",
+      "Lua",
+      "Luau",
+    ],
+    category: "Roblox",
+    github: "",
+    webapp: "",
+  },
+ /*member: [
+      {
+        name: "Rishav Chanda",
+        img: "https://avatars.githubusercontent.com/u/64485885?v=4",
+        linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+        github: "https://github.com/rishavchanda/",
+      },
+      {
+        name: "Upasana Chaudhuri",
+        img: "https://avatars.githubusercontent.com/u/100614635?v=4",
+        linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+        github: "https://github.com/upasana0710",
+      },
+    ],*/
+
+];
+
+export const TimeLineData = [
+  { year: 2020, text: "Started learn" },
+];
